0674f8d8324d4afaa8aa9834b0bdb0b7cf9d03a0
JBen93 <joshua.benjamin@ufl.edu>
2026-01-14 09:18:44 -0500

    made changes

--- Bayesian statistics/R scripts/Probability mass function.R
+++ Bayesian statistics/R scripts/Probability mass function.R
@@ -13,3 +13,12 @@ prob=dpois(k,lambda=5)
 for (i in 1:length(k)){
   lines(rep(k[i],2)+0.1,c(0,prob[i]),col='red',lwd=2)  
 }
+
+#plot a histogram of random variables that follow a beta distribution with parameters a=3 and b=3
+n=10000
+x=rbeta(n,shape1=3,shape2=3)
+hist(x,probability=T)
+
+#plot the theoretical beta distribution assuming that the parameters are a=3 and b=3
+seq1=seq(from=0,to=1,length.out=1000)
+lines(seq1,dbeta(seq1,shape1=3,shape2=3),col='red')
